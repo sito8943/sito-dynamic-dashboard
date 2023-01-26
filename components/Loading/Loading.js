77797f0a7854ffb5fdeb2302269b9fd4c6ab590d
Sito8943 <sito8943@gmail.com>
2023-01-26 10:27:50 -0500

    doing autocompleter

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -12,12 +12,27 @@ const Loading = (props) => {
       className={`flex w-full h-full items-center justify-center ${
         styles[`type-${type}`]
       }`}
-    ></div>
+    >
+      <div className={styles["sk-circle"]}>
+        <div className={`${styles["sk-circle1"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle2"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle3"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle4"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle5"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle6"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle7"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle8"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle9"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle10"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle11"]} ${styles["sk-child"]}`}></div>
+        <div className={`${styles["sk-circle12"]} ${styles["sk-child"]}`}></div>
+      </div>
+    </div>
   );
 };
 
 Loading.propTypes = {
-  type: PropTypes.number,
+  type: PropTypes.string,
 };
 
 export default Loading;
--- layout/dashboard/subs/Form.js
+++ layout/dashboard/subs/Form.js
@@ -25,6 +25,8 @@ const Form = ({ model }) => {
         newInputsValue[id] = newValue;
         return newInputsValue;
       }
+      case "clear":
+        return {};
       default:
         return { ...inputsState };
     }
@@ -40,8 +42,38 @@ const Form = ({ model }) => {
     [inputs]
   );
 
+  const modelsReducer = (modelsState, action) => {
+    const { type } = action;
+    switch (type) {
+      default:
+        return { ...modelsState };
+    }
+  };
+
+  const [models, setModels] = useReducer(modelsReducer, {
+    provinces: ["HOla", "Jelou"],
+  });
+
+  const getFilter = useCallback(
+    (field) => {
+      const { id } = field;
+      if (models[id] && inputs[id])
+        return models[id]
+          .filter(
+            (item) => item.toLowerCase().indexOf(inputs[id].toLowerCase()) >= 0
+          )
+          .map((jtem, i) => (
+            <button type="button" key={i}>
+              Lorem Ipsum
+            </button>
+          ));
+      return [];
+    },
+    [inputs, models]
+  );
+
   return (
-    <form onSubmit={sendInputs}>
+    <form onSubmit={sendInputs} className="flex flex-col gap-10">
       {Object.values(model).map((item) => (
         <div key={item.id}>
           {item.type === "text" ? (
@@ -50,7 +82,7 @@ const Form = ({ model }) => {
               <input
                 type={item.text}
                 value={inputs[item.id] || ""}
-                className="rounded-20px p-active"
+                className="bg-dark-blood rounded-20px p-active bg-none w-full"
                 placeholder={item.placeholder}
                 onChange={(e) =>
                   setInputs({
@@ -62,11 +94,35 @@ const Form = ({ model }) => {
               />
             </div>
           ) : null}
+          {item.type === "autocomplete" ? (
+            <div className={`${styles.autocomplete} flex flex-col gap-10`}>
+              <label htmlFor={item.id}>{labels[item.id]}</label>
+              <input
+                type={item.text}
+                value={inputs[item.id] || ""}
+                className="bg-dark-blood rounded-20px p-active w-full"
+                placeholder={item.placeholder}
+                onChange={(e) =>
+                  setInputs({
+                    id: item.id,
+                    type: "change",
+                    newValue: e.target.value,
+                  })
+                }
+              />
+              <div
+                className={`${styles.options} bg-blood rounded-20px flex flex-col gap-10`}
+              >
+                {getFilter(item)}
+              </div>
+            </div>
+          ) : null}
         </div>
       ))}
       <div className="flex gap-10 mt-5 w-full justify-end">
         <button
           type="button"
+          onClick={() => setInputs({ type: "clear" })}
           className={`${styles["no-active"]} ${styles["tab-button"]}`}
         >
           {buttons.clear}
