9be40a2150b157a44479899212bbebc499918709
Sito8943 <sito8943@gmail.com>
2023-03-18 23:21:01 -0700

    list of ids

--- pages/api/pharmaceuticGroups/list.js
+++ pages/api/pharmaceuticGroups/list.js
@@ -11,7 +11,9 @@ import { fetchTable } from "../../../lib/driver";
 export default async function handler(req, res) {
   try {
     const pharmaceuticGroups = await fetchTable("pharmaceuticGroups");
-    res.status(200).json({ pharmaceuticGroups: Object.values(pharmaceuticGroups) });
+    res
+      .status(200)
+      .json({ pharmaceuticGroups: Object.values(pharmaceuticGroups) });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: String(err) });
